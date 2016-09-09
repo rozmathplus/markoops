4a855a2323559036898258a3f2aacef348693cff
Yaroslav Trach <slavik@pixelant.se>
2016-09-09 14:56:59 +0300

    [UPDATE] Image min

--- grunt/imagemin.js
+++ grunt/imagemin.js
@@ -1,10 +1,14 @@
 module.exports = {
 	copyImages: {
+		options: {
+	        optimizationLevel: 3,
+	        svgoPlugins: [{ removeViewBox: false }]
+	    },
         files: [{
         	expand: true,
-	        cwd: '/',
-	        src: ['<%= dev%>/images/**/*.{svg,png,jpg,gif}'],
-	        dest: '<%= temp%>' 
+	        cwd: '<%= dev%>/images/',
+	        src: ['**/*.{svg,png,jpg,gif}'],
+	        dest: '<%= temp%>/images/' 
         }]
     }
 };
