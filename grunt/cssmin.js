5247a243c014cfc66409a03ec8e34098208debae
Yaroslav Trach <slavik@pixelant.se>
2016-09-19 17:05:06 +0300

    [FIX] CSS min

--- grunt/cssmin.js
+++ grunt/cssmin.js
@@ -1,13 +1,12 @@
+// https://github.com/gruntjs/grunt-contrib-cssmin
 module.exports = {
     options: {
+        shorthandCompacting: false,
+        roundingPrecision: -1
     },
     stylesFooter: {
-        target: {
-            files: [{
-                cwd: '<%= temp %>',
-                src: ['notPrioritizedBootstrap.css', 'notPrioritized.css'],
-                dest: 'general.css'
-            }]
-        }
+        files: [{
+            '<%= temp %>/general.css': ['<%= temp %>/notPrioritizedBootstrap.css', '<%= temp %>/notPrioritized.css']
+        }]
     }
 };
